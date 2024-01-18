476a819a1e8872c09729f7a2cd9285928a33746a
Brian Westphal <brian.westphal@panchaininc.com>
2024-01-18 16:01:15 +0800

    v2.1.9

--- src/register-ws-api-handler/types/InferWsApiRequestHandlerTypeFromApi.ts
+++ src/register-ws-api-handler/types/InferWsApiRequestHandlerTypeFromApi.ts
@@ -2,12 +2,9 @@ import type { WsApi } from 'yaschema-ws-api';
 
 import type { WsApiRequestHandler } from './WsApiRequestHandler';
 
-export type InferWsApiRequestHandlerTypeFromApi<
-  ApiT,
-  CommandNameT,
-  ExtraArgsT extends Record<string, any> = Record<string, never>
-> = ApiT extends WsApi<infer RequestCommandsT, infer ResponseCommandsT, infer QueryT>
-  ? CommandNameT extends keyof RequestCommandsT & string
-    ? WsApiRequestHandler<RequestCommandsT, ResponseCommandsT, CommandNameT, QueryT, ExtraArgsT>
-    : never
-  : never;
+export type InferWsApiRequestHandlerTypeFromApi<ApiT, CommandNameT, ExtraArgsT extends Record<string, any> = Record<string, never>> =
+  ApiT extends WsApi<infer RequestCommandsT, infer ResponseCommandsT, infer QueryT>
+    ? CommandNameT extends keyof RequestCommandsT & string
+      ? WsApiRequestHandler<RequestCommandsT, ResponseCommandsT, CommandNameT, QueryT, ExtraArgsT>
+      : never
+    : never;
